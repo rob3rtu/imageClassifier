3fa34309f59c53355eb7d631ebcd9f9de5910bbd
rob3rtu <36478750+rob3rtu@users.noreply.github.com>
2021-02-12 12:32:45 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+let mobilenet;
+let dropArea = document.getElementById('picture');
+let text = document.getElementById('text');
+let puffin = document.getElementById('puffin');
+
+if( window.innerWidth < window.innerHeight ) {
+    dropArea.style.width = 50 + 'vw';
+    dropArea.style.height = dropArea.style.width;
+} else {
+    dropArea.style.width = 50 + 'vh'
+    dropArea.style.height = dropArea.style.width;
+}
+
+dropArea.addEventListener('dragover', e => {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+});
+
+dropArea.addEventListener('drop', e => {
+    e.stopPropagation();
+    e.preventDefault();
+    
+    let files = e.dataTransfer.files;
+    for (let i = 0, file; file = files[i]; i++) {
+        if (file.type.match(/image.*/)) {
+            let reader = new FileReader();
+
+            reader.onload = e2 => {
+                // finished reading file data.
+                let img = document.createElement('img');
+                puffin.src= e2.target.result;
+            }
+            reader.readAsDataURL(file); // start reading the file data.
+        }
+    }
+
+    function modelReady() {
+        console.log('Model is ready!!');
+        mobilenet.predict(puffin, gotResults);
+    }
+    
+    function gotResults(error, results) {
+        if(error) {
+            console.log(error);
+        } else {
+            console.log(results);
+    
+            let ans = results[0].label;
+            console.log(ans);
+    
+            text.innerHTML = ans;
+        }
+    
+    }
+    
+    mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+});
---
+++ webcam/app.js
@@ -0,0 +1,100 @@
+let mobilenet;
+let videoZone = document.getElementById('video');
+let video = document.getElementById('videoElement');
+let text = document.getElementById('text');
+let flipBtn = document.getElementById('flip');
+
+if( window.innerWidth < window.innerHeight ) {
+    videoZone.style.width = 50 + 'vw';
+    videoZone.style.height = videoZone.style.width;
+
+    video.style.height = 50 + 'vw';
+    video.style.width = video.style.height;
+} else {
+    videoZone.style.width = 50 + 'vh'
+    videoZone.style.height = videoZone.style.width;
+
+    video.style.height = 50 + 'vh';
+    video.style.width = video.style.height;
+}
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Something went wrong!");
+      });
+  }
+
+  function modelReady() {
+    console.log('Model is ready!!');
+    mobilenet.predict(gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.log(error);
+    } else {
+       // console.log(results);
+
+        let ans = results[0].label;
+        //console.log(ans);
+
+        text.innerHTML = ans;
+
+        mobilenet.predict(gotResults);
+    }
+
+}
+
+mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
+
+// default user media options
+let defaultsOpts = { audio: false, video: true }
+let shouldFaceUser = true;
+
+// check whether we can use facingMode
+let supports = navigator.mediaDevices.getSupportedConstraints();
+if( supports['facingMode'] === true ) {
+  flipBtn.disabled = false;
+}
+
+let stream = null;
+
+function capture() {
+  defaultsOpts.video = { facingMode: shouldFaceUser ? 'user' : 'environment' }
+  navigator.mediaDevices.getUserMedia(defaultsOpts)
+    .then(function(_stream) {
+      stream  = _stream;
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(function(err) {
+      console.log(err)
+    });
+}
+
+flipBtn.addEventListener('click', function(){
+  if( stream == null ) return
+  // we need to flip, stop everything
+  stream.getTracks().forEach(t => {
+    t.stop();
+  });
+  // toggle / flip
+  shouldFaceUser = !shouldFaceUser;
+  
+  if( video.classList.contains('mirror') ) {
+      video.classList.remove('mirror');
+      video.classList.add('mirroroff');
+  } else if( video.classList.contains('mirroroff') ){
+    video.classList.remove('mirroroff');
+    video.classList.add('mirror');
+  }
+  
+  //console.log('ok');
+  capture();
+})
+
+capture();
